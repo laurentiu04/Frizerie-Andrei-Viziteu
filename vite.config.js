16220caf8ffc7e4c97db7829874de0f99af92c11
Laslau <laurentiulaurentiu04@gmail.com>
2025-12-07 11:12:14 +0000

    changed vite base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-	base: "/Frizerie-Andrei-Viziteu",
+	base: "/Frizerie-Andrei-Viziteu/",
 	plugins: [react()],
 });
