625786bdd415f9d487aee05ac31d1e2706ac2f85
Laslau <laurentiulaurentiu04@gmail.com>
2025-12-12 15:52:00 +0000

    wtf2

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 // server/server.js
 
 // 1. Load environment variables immediately
-// require("dotenv").config({ path: __dirname + "/.env" });
+require("dotenv").config({ path: __dirname + "/.env" });
 
 const express = require("express");
 const mongoose = require("mongoose");
@@ -60,7 +60,7 @@ const PORT = process.env.PORT || 5000;
 
 // Connect to the DB first, then start the Express server
 connectDB().then(() => {
-	app.listen(PORT, () => {
+	app.listen(PORT, "0.0.0.0", () => {
 		console.log(`Server listening on port:${PORT}`);
 	});
 });
